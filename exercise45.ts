55f0440bb74b5db76ec30ff30763ea96d05f5414
Badi1494 <hamidijaz1234@gmail.com>
2023-10-01 09:11:35 +0500

    update

--- exercise45.ts
+++ exercise45.ts
@@ -2,3 +2,20 @@
 Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
 
  */
+
+ function createCar(manufacturer: string, modelName: string, ... properties: [string,any][]):any
+ {
+    const car : any = {
+        manufacturer,
+        modelName,
+    }
+
+    for(const [Key, value ] of properties) {
+        car[Key] = value;
+    }
+    return car;
+ }
+
+ const myCar = createCar("Toyota", "Corolla", ["color","red"], ["optional feature", "sunroof"], ["speed", "240km/hour"])
+
+ console.log(myCar)
